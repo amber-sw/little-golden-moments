4c9132df5c01f02c185daa2f6acc7d5c50c4aefe
Amber Swevers <amber.swevers@hotmail.com>
2025-07-14 13:45:54 +0200

    sidenav working

--- src/app/shared/jumbotron/jumbotron.ts
+++ src/app/shared/jumbotron/jumbotron.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './jumbotron.css'
 })
 export class Jumbotron {
+  public menuState = false;
+
+  constructor(){};
+
+
+  toggleMenu() {
+    this.menuState = !this.menuState;
+    console.log(this.menuState);
+  }
 
 }
