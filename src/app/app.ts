7b54878948050a9faa579b1115f6ed30c9176fe9
Amber Swevers <amber.swevers@hotmail.com>
2025-07-15 13:24:27 +0200

    services section on home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Jumbotron } from './shared/jumbotron/jumbotron';
 import { Footer } from './footer/footer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Jumbotron, Footer],
+  imports: [RouterOutlet, Jumbotron, Footer, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -7,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.css'
 })
 export class HomePage {
+  public selectedService = 0;
+
+  /**
+   * Hier staan de verschillende pakketten opgelijst. 
+   * 'name' en 'description' mag je vrij aanpassen. 
+   * 'id' NIET AANPASSEN. 
+   * Indien je een bepaalde foto wilt aanpassen, vervang deze dan is de /src/assets folder door een foto met de dezelfde naam en extensie. Indien de extensie anders is pas deze dan hieronder aan bij 'src' of vraag aan mij om dit te veranderen.
+   * 
+   * Als je een extra pakket wilt toevoegen vraag je dit best aan ons. Om het zelf te doen kopieer je de onderstaande blok code en plak je hem op lijn 45 onder de },
+   * 
+  *       {
+            'id':3,
+            'name': 'NAAM PAKKET',
+            'description': 'BESCHRIJVING PAKKET',
+            'src': '/assets/naam-foto.jpg'
+          },
+   */
+  public services = [
+    {
+      'id':0,
+      'name': 'Huwelijk',
+      'description': 'huweljkLorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      'src': '/assets/wedding-example-home.jpg'
+    },
+    {
+      'id':1,
+      'name': 'Koppel',
+      'description': 'koppelLorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      'src': '/assets/couple-example-home.jpg'
+    },
+    {
+      'id':2,
+      'name': 'Familie',
+      'description': 'familieLorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      'src': '/assets/family-example-home.jpg'
+    },
+  ]
+  constructor(){}
+
+  changeSelection(id:number) {
+    this.selectedService = id;
+  }
 
 }
